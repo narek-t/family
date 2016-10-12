ce0cdd0b406ec566a20d2f8124806974bd578ce0
Narek <narek@Nareks-iMac.local>
2016-10-12 09:57:25 +0300

    all

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+$(document).ready(function() {
+	$('.open-video').magnificPopup({
+      type: 'iframe',
+      mainClass: 'my-mfp-slide-bottom',
+      removalDelay: 160,
+      preloader: false,
+      fixedBgPos: true,
+      fixedContentPos: true
+    });
+    $(".main-nav__mobile").click(function(event) {
+    	event.preventDefault();
+    	$(".main-nav__list").toggleClass('active');
+    });
+});
